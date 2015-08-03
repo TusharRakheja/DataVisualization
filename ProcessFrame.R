08166e8e5ccda07793d9e8f416e6b4c2c8c0b41d
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-08-03 11:35:40 +0530

    output.csv/txt -> Output.csv/txt

--- ProcessFrame.R
+++ ProcessFrame.R
@@ -1,11 +1,11 @@
 processFrame <- function() {
-     frame <- read.table("output.txt", sep = "\t")
+     frame <- read.table("Output.txt", sep = "\t")
      frame <- frame[, !names(frame) == "V3"]
      frame <- frame[!attributes(frame)$row.names == 1, ]
      attributes(frame)$names <- c("Flight dates", "Deaths")
      frame[, 1] <- as.Date(frame[, 1], "%d-%b-%Y")
      frame[, 1] <- format(frame[, 1], "%Y")
-     frame2 <- read.csv("output.csv", colClasses = "numeric")
+     frame2 <- read.csv("Output.csv", colClasses = "numeric")
      frame[, 2] <- frame2[,1]
      frame
 }
