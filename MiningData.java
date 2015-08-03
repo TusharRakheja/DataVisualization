971663666cfa3c3c6719cbd916b8952e5aafb09a
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-08-03 11:30:15 +0530

    Generating as much as needed now.

--- MiningData.java
+++ MiningData.java
@@ -4,8 +4,7 @@
  * 
  * Mined data classes for each accident:
  * - Date (String)
- * - Location (String)
- * - Passengers on board and Fatalities (both int)
+ * - Fatalities (int)
  *  
  * Data is then exported to a .csv file.
  * 
@@ -19,10 +18,10 @@ import java.io.*;
 public class MiningData {
     public static void main(String[] args) throws IOException {
         String urlFeed = new String("http://planecrashinfo.com/");  // Base.
-        Integer year = 1920;  // Dataset begins.
-        File output = new File("output.csv");                                // Declare a new File variable.
+        Integer year = 1950;  // Dataset begins.
+        File output = new File("Output.csv");                                // Declare a new File variable.
         if(output.exists()) {
-            System.out.print("The File already exists.");
+            System.out.print("The file already exists.");
         }
         else {
            output.createNewFile();                                           // Create output.txt
@@ -40,26 +39,17 @@ public class MiningData {
             while (readData.hasNext()) {
                 data.add(readData.nextLine());
             }            
-            //boolean readLocation = false;  // Location immediately follows date.
             for (String str : data) {
-                /*if (readLocation && str.contains("<br>")) {  // Location contains two parts sep. by <br> tag. Get first.
-                    String[] str2 = str.split("<br>"); 
-                    str2 = str2[0].split(">");  // Skip the tags for alignment and font.
-                    str2 = str2[2].split(",");
-                    out.print(str2[1]+ ", ");
-                    readLocation = false;
-                }*/
                 if (str.contains(".htm\">")) {  // Reading date (Found by reading sample data).
                     String[] str2 = str.split(">");
                     out.print(str2[3].substring(0, 11) + ", ");
-                    //readLocation = true;
                 }
                 else if (str.contains("width=\"1\"")) { // Distinguishing feature of fatalities.
                     String[] str2 = str.split("width=\"1\"");
                     str2 = str2[1].split(">");
                     str2 = str2[2].split("[(]");
                     str2 = str2[0].split("[/]");
-                    out.print(str2[0] + ", " + str2[1] + "\n");
+                    out.print(str2[0] + "\n");
                 }
             }
             year++;
