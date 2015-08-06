200ca83805e60f44099db228f6e43136adc25915
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-08-06 21:00:56 +0530

    Updated to make startYear and endYear variable.

--- DeathsPerYear.R
+++ DeathsPerYear.R
@@ -3,21 +3,24 @@
 
 # Returns a matrix mapping years to the number of deaths in those years.
 
-deathsPerYear <- function(frame) {
-     retFrame <- matrix(nrow = 0, ncol = 2)
-     year <- 1950                            # Data starts from year 1950.
-     y <- 2
+deathsPerYear <- function(frame, startYear = 1908, endYear = 2015) {
+     returnMatrix <- matrix(nrow = 0, ncol = 2)
+     year <- startYear                           # Data starts.
+     y <- 1
      sum <- 0
-     while (year <= 2015 && y <= 4584) {
+     while (year <= endYear && y <= nrow(frame)) {
           if (year == frame[y, 1]) {
                sum <- sum + as.integer(frame[y, 2])
                y <- y + 1
           } 
           else {
-               retFrame <- rbind(retFrame, c(year, sum))
+               returnMatrix <- rbind(returnMatrix, c(year, sum))
                year <- frame[y, 1]
                sum <- 0
           }
      }
-     retFrame       # Actually a matrix. 
+     ## To make a data frame: retFrame <- as.data.frame(retFrame)       
+     ## attributes(retFrame)$names <- c("Year", "Deaths")
+     write.csv(returnMatrix, "Plot Data.csv")
+     returnMatrix
 }
