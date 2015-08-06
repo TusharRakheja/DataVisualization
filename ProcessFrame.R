ab4f556d72bb044b5ff95524df3e4acd55503ab3
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-08-06 21:01:26 +0530

    Now supports custom filename.

--- ProcessFrame.R
+++ ProcessFrame.R
@@ -4,14 +4,12 @@
 # Returns a data frame mapping deaths in various accidents to the years of those
 # respective accidents.
 
-processFrame <- function() { 
-     frame <- read.table("Output.txt", sep = "\t")  
-     frame <- frame[, !names(frame) == "V3"]         # Delete extra column.
-     frame <- frame[!attributes(frame)$row.names == 1, ]
+processFrame <- function(file) { 
+     frame <- read.table(file, sep = "\t")
      attributes(frame)$names <- c("Flight dates", "Deaths")
      frame[, 1] <- as.Date(frame[, 1], "%d-%b-%Y")   # Convert to Date vs Deaths.
      frame[, 1] <- format(frame[, 1], "%Y")          # Convert to Year vs Deaths.
-     frame2 <- read.csv("Output.csv", colClasses = "numeric")
-     frame[, 2] <- frame2[,1]
+     frame[, 2] <- as.character(frame[, 2])
+     frame[, 2] <- as.numeric(frame[,2])
      frame
 }
