22b910f88e120511c2c3d94d38e6cb9566fa9c63
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-08-10 03:25:42 +0530

    Renamed and moved into the src folder.

--- Plot Distribution.R
+++
@@ -1,7 +0,0 @@
-# file is the path of the file containing the dolphin association data.
-
-plotDistribution <- function(file) {
-     frame <- read.table(file, sep = "\t", colClasses = "numeric")
-     attributes(frame)$names <- c("Network Size (#Associations)", "Frequency")
-     plot(frame)
-}
