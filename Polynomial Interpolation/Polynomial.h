6e88b97f0030ffeac0175cb93c91159178f06e9e
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-09-05 04:12:50 -0500

    The Polynomial class so far.

---
+++ Polynomial Interpolation/Polynomial.h
@@ -0,0 +1,23 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Polynomial {
+private:
+	struct Matrix {								// Used to store the augmented matrix for the data set.
+		float *column;
+	};
+	Matrix *row;								// The augmented matrix.
+	int n;										// The number of data points (= degree of polynomial).
+	void Gauss(Matrix *);						// Performs Gaussian elimination on the augmented matrix.
+	void Jordan(Matrix *);						// Converts the REF Matrix obtained from Gauss() to RREF.
+public:
+	Polynomial(string);							// Gets the data set from the filename (string) and generates the augmented matrix.
+};
+
+#endif
