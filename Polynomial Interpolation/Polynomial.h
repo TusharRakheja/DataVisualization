0849660f7e26f763b952518d2f52fd393d6b40e0
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-09-05 19:49:53 -0500

    Added operators to Polynomial and Polynomial::Matrix

--- Polynomial Interpolation/Polynomial.h
+++ Polynomial Interpolation/Polynomial.h
@@ -9,15 +9,22 @@ using namespace std;
 
 class Polynomial {
 private:
-	struct Matrix {								// Used to store the augmented matrix for the data set.
+	struct Matrix {											 // Used to store the augmented matrix for the data set.
 		float *column;
+		const float& operator[](int index) const {			 // Overloaded [] operator for const r-value access.
+			return this->column[index];
+		}
+		float& operator[](int index) {						 // Overloaded [] operator for l-value access.
+			return this->column[index];
+		}
 	};
-	Matrix *row;								// The augmented matrix.
-	int n;									// The number of data points (= degree of polynomial).
-	void Gauss(Matrix *);							// Performs Gaussian elimination on the augmented matrix.
-	void Jordan(Matrix *);						// Converts the REF Matrix obtained from Gauss() to RREF.
+	Matrix *matrix;											 // The augmented matrix.
+	int n;													 // The number of data points (= degree of polynomial).
+	void Gauss();											 // Performs Gaussian elimination on the augmented matrix.
+	void Jordan();											 // Converts the REF Matrix obtained from Gauss() to RREF.
 public:
-	Polynomial(string);							// Gets the data set from the filename (string) and generates the augmented matrix.
+	Polynomial(string);										 // Gets the data set from the filename (string) and generates the augmented matrix.
+	friend ostream& operator<<(ostream&, const Polynomial&); // To output the matrix.
 };
 
 #endif
