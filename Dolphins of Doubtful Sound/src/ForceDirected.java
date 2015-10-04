915ea08738ccc6752df04b6e767ac0baa8c1276a
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-10-04 01:30:43 -0500

    Starting file.

---
+++ Dolphins of Doubtful Sound/src/ForceDirected.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Class to make a force-directed graph of the data. 
+ * @author <i> Tushar </i>
+ */
+public class ForceDirected {
+    /**
+     * The main method.
+     * @param args Conventional.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
