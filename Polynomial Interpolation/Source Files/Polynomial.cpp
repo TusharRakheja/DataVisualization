73d6ee36d6bdadba610c7eae83515eea4935c45f
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-10-18 23:09:01 -0500

    Scale set, plots perfectly now! ^_^

--- Polynomial Interpolation/Source Files/Polynomial.cpp
+++ Polynomial Interpolation/Source Files/Polynomial.cpp
@@ -13,19 +13,37 @@
 #define SC_XOFFSET 80
 #define SC_YOFFSET 31
 
+/*
+ * Maps a pixel to its corresponding x value.
+ * Helper function for Polynomial::Plot().
+ */
+double xAtPixel(int pixel, double hx, double lx) {	
+	return (pixel*(hx - lx) + (SC_WIDTH*lx - 30 * hx)) / (SC_WIDTH - 30);
+}
+
+
+/*
+ * Maps a y value to its corresponding pixel.
+ * Helper function for Polynomial::Plot().
+ */
+int pixelAtY(double y, double hy, double ly) {
+	return (SC_HEIGHT - 30)*(hy - y) / (hy - ly);
+}
+
 Polynomial::Polynomial(string filename) {
 	fstream *file = new fstream(filename);
-	string line;
 	int count = 0;
+	string line;
 	while (getline(*file, line, '\n')) count++;				    // Count the number of points ..
 	this->n = count;							    // .. and assign that value to n.
-	file->clear();								    // Clear eol bit.
+	file->clear();								    // Clear eof bit.
 	file->seekp(0L, ios::beg);						    // Go to the beginning of the file. We can start reading data again now.
 	matrix = new Matrix[n];							    // Make an array of rows.
 	for (int i = 0; i < n; i++) {
 		matrix[i].column = new float[n + 1] withZeros;	// Set up a row containing all zeros.
 		float x_i, y_i;
-		*file >> x_i >> y_i;						    // Read one data point.
+		*file >> x_i >> y_i;
+		//cout << x_i << " " << y_i << endl;
 		if (i == 0) {
 			hx = lx = x_i;
 			hy = ly = y_i;
@@ -44,6 +62,7 @@ Polynomial::Polynomial(string filename) {
 	file->close();
 	delete file;								    // Not deleting the file, just the fstream object.
 	Gauss(); Jordan();       					            // Call the functions to perform Gauss-Jordan elimination to get a_i.
+	cout << *this;
 	Plot();
 }
 
@@ -62,12 +81,12 @@ void Polynomial::Gauss() {
 	}
 }
 
-void Polynomial::Jordan() {						
+void Polynomial::Jordan() {
 	for (int i = n - 1; i >= 0; i--) {
 		for (int row = i - 1; row >= 0; row--) {
 			float multiplier = matrix[row][i];		 	    // Since in row 'i' of the matrix, the only non-zero element left is 1.
 			matrix[row][i] -= multiplier;
-			matrix[row][n] -= multiplier;
+			matrix[row][n] -= multiplier*matrix[i][n];
 		}
 	}
 }
@@ -93,14 +112,12 @@ void Polynomial::Plot() {
 	r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
 	SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);        // Make the screen
 	SDL_RenderClear(r);												   // white.
+	double xScale = (SC_WIDTH - 30) / (hx - lx);
+	double yScale = (SC_HEIGHT - 30) / (hy - ly);
 	SDL_SetRenderDrawColor(r, 0, 0, 0, 50);
 	SDL_RenderDrawLine(r, 0, SC_HEIGHT-30, SC_WIDTH, SC_HEIGHT-30);	   // X axis.
 	SDL_RenderDrawLine(r, 30, 0, 30, SC_HEIGHT);					   // Y-axis.
 	SDL_SetRenderDrawColor(r, 150, 150, 150, SDL_ALPHA_OPAQUE);
-	double yScale = (SC_HEIGHT - 30)/(hy - ly);
-	double xScale = (SC_WIDTH - 30)/(hx - lx);
-	cout << "X Scale: " << xScale << endl;
-	cout << "Y Scale: " << yScale << endl;
 	for (double i = 30 + 30; i < SC_WIDTH; i += 30) {				   // Set up the vertical grid-lines.
 		SDL_RenderDrawLine(r, i, 0, i, SC_HEIGHT);
 	}
@@ -110,11 +127,11 @@ void Polynomial::Plot() {
 	SDL_SetRenderDrawColor(r, 255, 0, 0, SDL_ALPHA_OPAQUE);			   // The curve will be red in color.
 	double delX = ((double)(SC_WIDTH - 30))/1000.0;					   // The difference between two consecutive xs. 	
 	SDL_Rect point;
-	for (double x = lx; x <= hx; x+=0.001) {
-		point.x = 30 + x*xScale;
-		point.y = this->of(x)*yScale;
-		point.w = 2;
-		point.h = 2;
+	for (int pixel = 30; pixel <= SC_WIDTH; pixel++) {
+		double x = xAtPixel(pixel, hx, lx);
+		point.x = pixel;
+		point.y = pixelAtY(this->of(x), hy, ly);
+		point.w = point.h = 1;
 		SDL_RenderFillRect(r, &point);									// Color point.
 	}
 	SDL_RenderPresent(r);
