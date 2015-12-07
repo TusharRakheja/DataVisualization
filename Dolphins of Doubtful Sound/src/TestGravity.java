b29aebbe315fe7a8343a378b33f832172884bca2
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-12-06 23:48:22 -0600

    graph() method. Neat.

--- Dolphins of Doubtful Sound/src/TestGravity.java
+++ Dolphins of Doubtful Sound/src/TestGravity.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 
 public class TestGravity {
     public static void main(String[] args) {
@@ -9,9 +10,9 @@ public class TestGravity {
         dolphin[0].velocity(new Vector2D(0, -0.1));
         dolphin[1].velocity(new Vector2D(0, 0.1));
         dolphin[0].addToNetwork(dolphin[1]); 
-        info(dolphin);   
-        //graph();
-        simulate(dolphin);
+        info(dolphin);
+        graph(dolphin);
+        //simulate(dolphin);
     }
     public static void info(Dolphin[] dolphin) {
         System.out.println("M1: " + dolphin[0].mass() + "\tM2: " + dolphin[1].mass());
@@ -68,18 +69,79 @@ public class TestGravity {
             if (cForce2 > centriMax2) {
                 centriMax2 = cForce2;
             }
+            //dolphin[1].eraseNode2(0.02, 0.02);
+            //dolphin[0].eraseNode2(0.02, 0.02);
         }
         System.out.println("Max Gravity: " + graviMax + "\tMin Gravity: " + graviMin);
         System.out.println("Max Centri1: " + centriMax1+"\tMin Centri1: " + centriMin1);
         System.out.println("Max Centri2: " + centriMax2+"\tMin Centri2: " + centriMin2);
     }
-    public static void graph() {
-        StdDraw.setCanvasSize(800, 670);
-        StdDraw.setXscale(0, 800);
-        StdDraw.setYscale(0, 670);
-        StdDraw.line(30, 0, 30, 670);
-        StdDraw.line(0, 30, 800, 30);
-        StdDraw.text(18, 15, "0");
+    public static void graph(Dolphin[] dolphin) {
+        int yUp = 670, xUp = 800;
+        double timeStep = 1.0;
+        int xOffset = 60, yOffset = 60;
+        double st = 0, t = 0, et = 8000;        
+        double minForce = 0.0;
+        double maxForce = 0.025;
+        int fontSize = 9;
+        int legendYOffset = 20;
+        int legendXOffset = 50;
+        StdDraw.setCanvasSize(xUp, yUp + 10);
+        StdDraw.setXscale(0, xUp);
+        StdDraw.setYscale(0, yUp);
+        StdDraw.line(xOffset, 0, xOffset, yUp);               // Y axis.
+        StdDraw.line(0, yOffset, xUp, yOffset);               // X axis.
+        Font f = new Font("Sans-Serif", Font.PLAIN, fontSize);
+        StdDraw.text(xOffset - 2*fontSize, yOffset - 2*fontSize, "0");
+        StdDraw.setFont(f);
+        for (int i = xOffset + 50; i <= xUp; i += 50) {
+            double time = ((((i - xOffset)*(et - st))/(xUp - xOffset)) + st);
+            int Time = (int) time;
+            StdDraw.text(i, yOffset - 2*fontSize, Double.toString(Time));
+        }
+        for (int j = yOffset + 25; j <= yUp; j += 25) {
+            double force = ((((j - yOffset)*(maxForce - minForce))/(yUp - yOffset)) + minForce)*1000;
+            int Force = (int) force;
+            StdDraw.text(xOffset - 2*fontSize, j, Double.toString(Force));
+        }
+        StdDraw.setFont(new Font("Sans-Serif", Font.PLAIN, (int)(1.5*fontSize)));
+        StdDraw.text((xUp + xOffset)/2, yOffset - 4.5*fontSize, "Time(s) ->");
+        StdDraw.text(xOffset - 4.5*fontSize, (yUp + yOffset)/2, "Force(mN) ->", 90);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.line(xUp - 3*legendXOffset, yUp - legendYOffset, xUp - 2*legendXOffset, yUp - legendYOffset);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.line(xUp - 3*legendXOffset, yUp - 2*legendYOffset, xUp - 2*legendXOffset, yUp - 2*legendYOffset);
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.line(xUp - 3*legendXOffset, yUp - 3*legendYOffset, xUp - 2*legendXOffset, yUp - 3*legendYOffset);
+        StdDraw.text(xUp - legendXOffset, yUp - legendYOffset, "Gravity");
+        StdDraw.text(xUp - legendXOffset, yUp - 2*legendYOffset, "Centripetal 1");
+        StdDraw.text(xUp - legendXOffset, yUp - 3*legendYOffset, "Centripetal 2");
+        while (t <= et) {
+            if (StdDraw.mousePressed()) {
+                break;
+            }
+            double gForce = dolphin[0].mass()*dolphin[1].mass()/Dolphin.distSquared(dolphin[1],dolphin[0]);
+            double cForce1 = dolphin[0].mass()*dolphin[0].velocity().lensq()/(Dolphin.dist(dolphin[1], dolphin[0]));
+            double cForce2 = dolphin[1].mass()*dolphin[1].velocity().lensq()/(Dolphin.dist(dolphin[1], dolphin[0]));
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.filledCircle((xUp - xOffset)*((t - st)/(et - st)) + xOffset, ((yUp - yOffset)/(maxForce - minForce))*(gForce - minForce) + yOffset, 0.002);
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.filledCircle((xUp - xOffset)*((t - st)/(et - st)) + xOffset, ((yUp - yOffset)/(maxForce - minForce))*(cForce1 - minForce) + yOffset, 0.002);
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.filledCircle((xUp - xOffset)*((t - st)/(et - st)) + xOffset, ((yUp - yOffset)/(maxForce - minForce))*(cForce2 - minForce) + yOffset, 0.002);
+            dolphin[0].accel(new Vector2D(0, 0));
+            dolphin[1].accel(new Vector2D(0, 0));
+            Vector2D newVec = new Vector2D(dolphin[0].position().minus(dolphin[1].position()).unit().
+                    times(dolphin[0].mass()*dolphin[1].mass()/Dolphin.distSquared(dolphin[1], dolphin[0])));
+            dolphin[1].accel().add(newVec);
+            newVec.reverse();
+            dolphin[0].accel().add(newVec);
+            dolphin[0].adjust(timeStep);
+            dolphin[1].adjust(timeStep);
+            t += timeStep;
+            //dolphin[1].drawNode2(0.01, 0.01);
+            //dolphin[0].drawNode2(0.01, 0.01);
+        }
     }
     public static void graphQuantity(double timeShift, double q1, double q2) {
     }
